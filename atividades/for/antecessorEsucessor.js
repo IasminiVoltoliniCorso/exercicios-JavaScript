7873b4fb2f2a7b925cf0675a61bac8b8cf5f5fae
IasminiVoltoliniCorso <86911103+IasminiVoltoliniCorso@users.noreply.github.com>
2021-08-25 19:52:25 +0100

    formatação

--- atividades/for/antecessorEsucessor.js
+++ atividades/for/antecessorEsucessor.js
@@ -1,12 +1,12 @@
-function antEsuc (number){
+function antEsuc(number) {
     let antecessor;
     let sucessor;
-    for (i = 0; i < number.length; i++){
-        if (number[i] != 0){
+    for (i = 0; i < number.length; i++) {
+        if (number[i] != 0) {
             antecessor = number[i] - 1
             sucessor = number[i] + 1
             console.log(`O antecessor de ${number[i]} é ${antecessor} e o sucessor é ${sucessor}`)
-        } 
+        }
     }
 }
 antEsuc([1, 2, 4, 5, 11, 55, 69, 100]);
